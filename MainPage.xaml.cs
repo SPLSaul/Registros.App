f33616848a6fb9affb8c21aaaeb88134b17ea2b1
Saul <Saul@DESKTOP-B2JBK0T>
2023-02-09 09:06:04 -0800

    Configurada MainPage

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,30 @@
-﻿namespace Registros;
+﻿using Microsoft.Maui.Controls.Shapes;
+
+namespace Registros;
 
 public partial class MainPage : ContentPage
 {
 	int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+    private void btnCine(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Cine());
+    }
+    private void btnMusica(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Musica());
+    }
+    private void btnLibros(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Libros());
+    }
+    private void btnEjercicio(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new Ejercicio());
+    }
 }
 
